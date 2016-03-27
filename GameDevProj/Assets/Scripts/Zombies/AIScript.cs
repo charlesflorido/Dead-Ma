319e7f704f8c4de0c3338cc74bd5849a5a997f06
Louiz Echiverri <louizechiverri@gmail.com>
2016-03-28 02:13:22 +0800

    created zombie prefab

---
+++ GameDevProj/Assets/Scripts/Zombies/AIScript.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIScript : MonoBehaviour 
+{
+	public Transform target;
+	public float moveSpeed;
+	public int rotationSpeed;
+	public AudioClip sound;
+	private int lifeline;
+
+	void Start()
+	{
+		lifeline = 3;
+	}
+
+
+	
+	void Update() 
+	{
+
+			GameObject go = GameObject.FindWithTag("Player");
+
+			if (go != null)
+			{
+				target = go.transform;
+			}
+
+			if (target == null)
+			{
+				return;
+			}
+
+			Vector3 dir = target.position - transform.position;
+
+
+			// Only needed if objects don't share 'z' value.
+			dir.z = 0.0f;
+			if (dir != Vector3.zero) 
+				transform.rotation = Quaternion.Slerp ( transform.rotation, 
+				                                       Quaternion.FromToRotation (Vector3.right, dir), 
+				                                       rotationSpeed * Time.deltaTime);
+			//Move Towards Target
+			transform.position += (target.position - transform.position).normalized 
+				* moveSpeed * Time.deltaTime;
+	}
+	
+
+	void OnCollisionEnter2D(Collision2D target)
+	{
+		if(target.gameObject.tag == "projectile")
+		{
+			AudioSource.PlayClipAtPoint(sound, this.gameObject.transform.position);
+			lifeline--;
+			if (lifeline == 0)
+				Destroy(gameObject);
+		}
+	}
+}

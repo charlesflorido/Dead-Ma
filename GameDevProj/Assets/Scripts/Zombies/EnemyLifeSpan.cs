f51a3ef029beacd2d31740eebb51f0c53b5bb48e
Louiz Echiverri <louizechiverri@gmail.com>
2016-04-03 21:48:36 +0800

    added minions

---
+++ GameDevProj/Assets/Scripts/Zombies/EnemyLifeSpan.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyLifeSpan : MonoBehaviour {
+
+	public int hitPoints;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnCollisionEnter2D(Collision2D target) {
+		if(target.gameObject.tag == "projectile") {
+			hitPoints--;
+		}
+		
+		if (hitPoints == 0) {
+			GameObject.Destroy(gameObject);
+		}
+	}
+}
